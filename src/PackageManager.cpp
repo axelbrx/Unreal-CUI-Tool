46496f2502206644469aaccbb231cac04a61a6cc
Axel Bureaux <bureaux.axel@gmail.com>
2025-02-03 19:11:48 -0500

    [+] Added package OS

--- src/PackageManager.cpp
+++ src/PackageManager.cpp
@@ -8,10 +8,11 @@ void PackageManager::Package(const std::string& uprojectPath, const std::string&
     // Construire la commande de package en fonction du système d'exploitation
     #ifdef _WIN32
         // Windows
-        std::cout << "\033[1;33mEtant sur Mac, je ne peux pas tester le package Windows, exécution quand même.\033[0m" << std::endl;
+        std::cout << "\033[1;33mEtant sur Mac, je ne peux pas tester le package Windows, exécution quand même.\n\033[0m" << std::endl;
         packageCommand = "./Engine/Build/BatchFiles/RunUAT.bat BuildCookRun -project=" + uprojectPath + " -noP4 -clientconfig=Shipping -serverconfig=Development -nocompile -nocompileeditor -installed -utf8output -platform=Win64 -build -cook -CookCultures=en -unversionedcookedcontent -stage -package -archive=" + outputPath;
     #elif __APPLE__
         // macOS
+        std::cout << "\033[1;33mExécution de la commande de package pour MacOS\n" << std::endl;
         packageCommand = "./Engine/Build/BatchFiles/RunUAT.sh BuildCookRun -project=" + uprojectPath + " -noP4 -clientconfig=Shipping -serverconfig=Development -nocompile -nocompileeditor -installed -utf8output -platform=Mac -build -cook -CookCultures=en -unversionedcookedcontent -stage -package -archive=" + outputPath;
     #else
         #error "Plateforme non supportée"
